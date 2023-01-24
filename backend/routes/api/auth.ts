e9ea9dd5f7df745b77aee3906eb56dbecd87639d
Bastien <etienne.bastien.08@gmail.com>
2023-01-24 22:13:36 +0100

    Backend update prisma

--- backend/routes/api/auth.ts
+++ backend/routes/api/auth.ts
@@ -6,5 +6,15 @@ auth.post("/login", async (ctx: any) => {
   const data = await ctx.req.json();
   return ctx.json({ message: data });
 });
+auth.post("/register", async (ctx: any) => {
+  console.log("on arrive dans register");
+});
+
+auth.post("/create-account", async (c) => {
+  const body = await c.req.json();
+  const res = await addUSer(body);
+  console.log(res);
+  return c.json({ message: res });
+});
 
 export default auth;
